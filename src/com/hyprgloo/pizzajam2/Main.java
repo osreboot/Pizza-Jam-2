1600fac4f2a32ca0643c61d07e733cf53f2e9cf9
osreboot <calvin.16.weaver@gmail.com>
2018-06-08 14:07:14 -0700

    Font

--- src/com/hyprgloo/pizzajam2/Main.java
+++ src/com/hyprgloo/pizzajam2/Main.java
@@ -1,6 +1,10 @@
 package com.hyprgloo.pizzajam2;
 
+import org.lwjgl.opengl.Display;
+import org.newdawn.slick.Color;
+
 import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.painter.painter2d.HvlFontPainter2D;
 import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
 
 public class Main extends HvlTemplateInteg2D{
@@ -9,17 +13,29 @@ public class Main extends HvlTemplateInteg2D{
 		new Main();
 	}
 	
+	public static final int
+	INDEX_FONT = 0;
+	
+	public static HvlFontPainter2D font;
+	
 	public Main(){
 		super(144, 1280, 720, "Pizza Jam 2 : HYPRGLOO Submission", new HvlDisplayModeDefault());
 	}
 
 	@Override
 	public void initialize(){
+		getTextureLoader().loadResource("INOF");
+		
+		font = new HvlFontPainter2D(getTexture(INDEX_FONT), HvlFontPainter2D.Preset.FP_INOFFICIAL);
+		font.setCharSpacing(16f);
+		
 		Game.initialize();
 	}
 
 	@Override
 	public void update(float delta){
+		font.drawWordc("GAME", Display.getWidth()/2, Display.getHeight()/2, Color.white);
+		
 		Game.update(delta);
 	}
 
