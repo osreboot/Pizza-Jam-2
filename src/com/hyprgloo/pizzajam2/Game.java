55f3bdb6072e49aa8863c28d90a71b4b3135b5bd
haveaniceday33 <munrosamuel@gmail.com>
2018-06-08 19:38:56 -0400

    sam commit #5

--- src/com/hyprgloo/pizzajam2/Game.java
+++ src/com/hyprgloo/pizzajam2/Game.java
@@ -12,20 +12,17 @@ public class Game {
 	public static ArrayList<LineSegment> mites;
 	public static ArrayList<PowerUp> powerUps;
 	
-	public static HvlCoord2D miteStart;
-	public static HvlCoord2D miteEnd;
-	public static HvlCoord2D titeStart;
-	public static HvlCoord2D titeEnd;
+	public static final float SCROLLSPEED = 100;
 	
 	public static float miteTimer;
 	public static float titeTimer;
 	
 	public static void initialize(){
 		player = new Player(Player.PLAYER_START_X, Player.PLAYER_START_Y);
-		miteStart = new HvlCoord2D();
-		miteEnd = new HvlCoord2D();
-		titeStart = new HvlCoord2D();
-		titeEnd = new HvlCoord2D();
+		HvlCoord2D miteStart = new HvlCoord2D();
+		HvlCoord2D miteEnd = new HvlCoord2D();
+		HvlCoord2D titeStart = new HvlCoord2D();
+		HvlCoord2D titeEnd = new HvlCoord2D();
 		tites = new ArrayList<LineSegment>();
 		mites = new ArrayList<LineSegment>();
 		miteTimer = 100;
@@ -46,6 +43,11 @@ public class Game {
 	
 
 	public static void genMite(float delta) {
+		
+		HvlCoord2D miteStart = new HvlCoord2D();
+		HvlCoord2D miteEnd = new HvlCoord2D();
+
+		
 		miteStart.x = mites.get(mites.size()-1).end.x;
 		miteStart.y = mites.get(mites.size()-1).end.y;
 		miteEnd.x = HvlMath.randomFloatBetween(1280, 1350);
@@ -55,6 +57,9 @@ public class Game {
 		miteTimer = HvlMath.randomFloatBetween(80, 120);
 	}
 	public static void genTite(float delta) {
+		HvlCoord2D titeStart = new HvlCoord2D();
+		HvlCoord2D titeEnd = new HvlCoord2D();
+		
 		titeStart.x = tites.get(tites.size()-1).end.x;
 		titeStart.y = tites.get(tites.size()-1).end.y;
 		titeEnd.x = HvlMath.randomFloatBetween(1280, 1350);
@@ -65,17 +70,17 @@ public class Game {
 	}
 	public static void update(float delta){
 		for(LineSegment miteWave : mites) {
-			miteWave.start.x -= delta*100;
-			miteWave.end.x -= delta*100;
+			miteWave.start.x -= delta*SCROLLSPEED;
+			miteWave.end.x -= delta*SCROLLSPEED;
 			miteWave.draw(delta);
 		}
 		for(LineSegment titeWave : tites) {
-			titeWave.start.x -= delta*100;
-			titeWave.end.x -= delta*100;
+			titeWave.start.x -= delta*SCROLLSPEED;
+			titeWave.end.x -= delta*SCROLLSPEED;
 			titeWave.draw(delta);
 		}
-		titeTimer -= delta*50;
-		miteTimer -= delta*50;
+		titeTimer -= delta*150;
+		miteTimer -= delta*150;
 		if(miteTimer <= 0) {
 			genMite(delta);
 		}
