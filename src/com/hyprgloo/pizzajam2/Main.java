9e3fab2bd2a0bc7f9ced9893c0f1952996fd019f
osreboot <calvin.16.weaver@gmail.com>
2018-06-08 12:49:22 -0700

    Basic template setup

--- src/com/hyprgloo/pizzajam2/Main.java
+++ src/com/hyprgloo/pizzajam2/Main.java
@@ -1,5 +1,26 @@
 package com.hyprgloo.pizzajam2;
 
-public class Main {
+import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
+
+public class Main extends HvlTemplateInteg2D{
+
+	public static void main(String[] args){
+		new Main();
+	}
+	
+	public Main(){
+		super(144, 1280, 720, "Pizza Jam 2 : HYPRGLOO Submission", new HvlDisplayModeDefault());
+	}
+
+	@Override
+	public void initialize(){
+		
+	}
+
+	@Override
+	public void update(float delta){
+		
+	}
 
 }
