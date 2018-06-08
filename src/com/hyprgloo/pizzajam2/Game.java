260b310f3da96df925dad5c2bb550ad212e2fbf1
osreboot <calvin.16.weaver@gmail.com>
2018-06-08 14:01:31 -0700

    Outlines

---
+++ src/com/hyprgloo/pizzajam2/Game.java
@@ -0,0 +1,17 @@
+package com.hyprgloo.pizzajam2;
+
+public class Game {
+	
+	public static void initialize(){
+		
+	}
+	
+	public static void update(float delta){
+		
+	}
+	
+	public static void restart(){
+		
+	}
+
+}
---
+++ src/com/hyprgloo/pizzajam2/Player.java
@@ -0,0 +1,21 @@
+package com.hyprgloo.pizzajam2;
+
+public class Player {
+
+	public Player(){
+
+	}
+
+	public void draw(float delta){
+
+	}
+
+	public float getX(){
+		return 0;
+	}
+
+	public float getY(){
+		return 0;
+	}
+
+}
