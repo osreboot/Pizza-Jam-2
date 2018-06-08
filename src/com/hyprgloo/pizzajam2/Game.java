8ceda0d15d0ba58902ddadd1a18ef6c064d620ea
haveaniceday33 <munrosamuel@gmail.com>
2018-06-08 18:38:30 -0400

    sam commit #1

--- src/com/hyprgloo/pizzajam2/Game.java
+++ src/com/hyprgloo/pizzajam2/Game.java
@@ -1,20 +1,29 @@
 package com.hyprgloo.pizzajam2;
 
+import java.util.ArrayList;
+
 public class Game {
 	
 	public static Player player;
+	public static ArrayList<LineSegment> tites;
+	public static ArrayList<LineSegment> mites;
 	
 	public static void initialize(){
 		player = new Player(Player.PLAYER_START_X, Player.PLAYER_START_Y);
 	}
 	
 	public static void update(float delta){
+		
+	
+		
 		player.update(delta);
 		player.draw(delta);
 	}
 	
 	public static void restart(){
 		player = new Player(Player.PLAYER_START_X, Player.PLAYER_START_Y);
+		tites.clear();
+		mites.clear();
 	}
 
 }
--- src/com/hyprgloo/pizzajam2/LineSegment.java
+++ src/com/hyprgloo/pizzajam2/LineSegment.java
@@ -1,15 +1,22 @@
 package com.hyprgloo.pizzajam2;
 
+import org.newdawn.slick.Color;
+
 import com.osreboot.ridhvl.HvlCoord2D;
+import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
 
 public class LineSegment {
+	
+	public HvlCoord2D start;
+	public HvlCoord2D end;
 
 	public LineSegment(HvlCoord2D start, HvlCoord2D end){
-		
+		this.start = start;
+		this.end = end;
 	}
 	
 	public void draw(float delta){
-		
+		HvlPainter2D.hvlDrawLine(this.start.x, this.start.y, this.end.x, this.end.y, Color.white);
 	}
 	
 }
